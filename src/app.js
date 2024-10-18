b88dfe046c590c4fa2f83e415101e4353dc523d6
wadhavane92@gmail.com <wadhavane92@gmail.com>
2024-10-18 18:11:17 +0530

    Setup Initial Server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.get("/abc", (req, res) => {
+    res.send("abc from server");
+  });
+
+app.listen(3000, () => console.log("listening on port 3000"));
