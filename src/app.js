0f91a667275a8d0e7f0b87cf86a7a6e69a7a94bb
wadhavane92@gmail.com <wadhavane92@gmail.com>
2024-10-19 17:11:19 +0530

    played with route handlers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
-});
+app.get("/user", (req, res,next) => {
+   console.log("first route handler")
+   next();
+    // res.send({firstname:"Vaibhav",lastName:"Wadhavane"});
+  },
+  (req, res,next) => {
+    console.log("Second route handler")
+     res.send("Second route handler");
+     next();
+   },
+   (req, res,next) => {
+    console.log("Third route handler")
+     res.send("Third route handler");
+   },
+);
 
-app.get("/user", (req, res) => {
-    res.send({firstname:"Vaibhav",lastName:"Wadhavane"});
-  });
-
-  app.post("/user",(req,res)=>{
-    res.send("Data saved to the database successfully")
-  })
+  
 
 app.listen(3000, () => console.log("listening on port 3000"));
 
