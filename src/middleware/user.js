d2947967a425905824d0bb4702223bfd20546412
wadhavane92@gmail.com <wadhavane92@gmail.com>
2024-10-21 11:02:35 +0530

    Writing-auth-middleware

---
+++ src/middleware/user.js
@@ -0,0 +1,13 @@
+const userAuth=(req,res)=>{
+    console.log("Checking User Authentication")
+     const token="abc";
+     const isAuthorized=token==="abc";
+     if(isAuthorized){
+      res.send("User Data sent")
+     }else{
+      res.send("Not A Authorized user")
+     }
+  }
+module.exports={
+    userAuth
+}
